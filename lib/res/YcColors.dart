b7bd51a39452595b0cad2f5ae99cdcdb0769851f
yangchong211 <yangchong211@163.com>
2019-06-14 09:36:31 +0800

    简单路由封装

--- lib/res/YcColors.dart
+++ lib/res/YcColors.dart
@@ -39,4 +39,17 @@ class YcColors {
   static Color colorIndigo = const Color(0xFF1A237E);
   static Color colorIndigo10 = const Color(0xFF3949AB);
   static Color colorIndigo20 = const Color(0xFF9FA8DA);
+
+
+  static const Color app_main = Color(0xff4688FA);
+  static const Color bg_color = Color(0xfff1f1f1);
+  static const Color text_dark = Color(0xFF333333);
+  static const Color text_normal = Color(0xFF666666);
+  static const Color text_gray = Color(0xFF999999);
+  static const Color text_gray_c = Color(0xFFcccccc);
+  static const Color bg_gray = Color(0xFFF6F6F6);
+  static const Color text_blue = Color(0xFF4688FA);
+  static const Color line = Color(0xFFEEEEEE);
+  static const Color order_line = Color(0xFFDDDDDD);
+  static const Color text_red = Color(0xFFFF4759);
 }
